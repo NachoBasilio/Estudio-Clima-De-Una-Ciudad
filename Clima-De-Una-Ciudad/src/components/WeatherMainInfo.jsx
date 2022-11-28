f92b7f71bf79323528bcce448aa1bdcc0af9ae9e
IgnaDev <arcortoon@gmail.com>
2022-11-28 14:31:19 -0300

    Completado, solo faltan estilos

--- Clima-De-Una-Ciudad/src/components/WeatherMainInfo.jsx
+++ Clima-De-Una-Ciudad/src/components/WeatherMainInfo.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 
 export default function WeatherMainInfo({weather}) {
-
+    console.log(weather)
     return (
         <>
             <div>{weather?.location.name}</div>
             <div>{weather?.location.country}</div>
             <div>
                 <div>
-                    <img src={`http:${weather?.current.condition.icon}`} width="128" alt={weather?.current.condition.text} />
+                    <img 
+                    src={`http:${weather?.current.condition.icon}`} 
+                    width="128" 
+                    alt={weather?.current.condition.text} 
+                    />
+                </div>
+                <div>
+                    <div>{weather?.current.condition.text}</div>
+                    <div>{weather?.current.temp_c}°</div>
                 </div>
             </div>
+            <iframe 
+            title='mapa'
+            src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14192.169188367!2d${weather?.location.lon}!3d${weather?.location.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1669656512888!5m2!1ses-419!2sar`}
+            width="600" 
+            height="450" 
+            style={{border:0}} 
+            allowfullscreen="" 
+            loading="lazy" 
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
         </>
     )
 }
